90cc04f4588e977f7114661a018dfc7d0df19f56
Chuksjohnleo <nwanonenyichukwuka@gmail.com>
2022-12-03 22:21:40 +0100

    new design

--- index.js
+++ index.js
@@ -1,103 +1,35 @@
-sidebar = document.getElementById('sidebar');
-const logs = document.getElementById('logs');
+const nav = document.getElementById('nav');
+const projects_btn = document.getElementById('projects');
+const contacts_btn = document.getElementById('contacts');
+const about_btn = document.getElementById('about');
+const nav_divs = document.getElementsByClassName('nav_btn');
+const email = document.getElementById('email');
+const about_me = document.getElementById('about_me');
+const projects = document.getElementById('project');
 
-const to_top = document.getElementsByClassName('to_top')[0];
-//the scroll tab
+function click_btn(){
+    for(let i=0; i<nav_divs.length;i++) {
+        let btn =  nav_divs[i];
+        btn.style.borderTop = '0px';
+    };
+};
 
-function topY() {
-  logs.scrollIntoView();
-  to_top.style.display = 'none';
-}
-window.onscroll = () => {
-  if (window.scrollY > 1000) {
-    to_top.style.display = 'block';
-  } else {
-    to_top.style.display = 'none';
-  }
-}
+for(let i=0; i<nav_divs.length;i++) {
+    let btn =  nav_divs[i];
+    btn.addEventListener('click',()=>{
+        click_btn();
+        btn.style.borderTop = '3px solid lightblue';
+    });
+};
 
-//the sidebar
-function options() {
-  /* sidebar.style.width = '30%' */
-  sidebar.classList.add('sidebar-mobile','padding');
-  document.getElementById('opt').textContent = '';
-}
+contacts_btn.addEventListener('click',()=>{
+    email.scrollIntoView();
+});
 
-function back() {
-  /*   sidebar.style.width = '' */
-  sidebar.classList.remove('sidebar-mobile','padding');
-  document.getElementById('opt').textContent = '≡';
-}
-//drawdown menus at the sidebar
-function drawDown() {
-  var abc = document.getElementById('tools');
-  abc.classList.toggle('drawUpandDown');
-  if (document.getElementById('upD').textContent === '►') {
-    document.getElementById('upD').textContent = '▼'
-  } else {
-    document.getElementById('upD').textContent = '►'
-  }
-}
-var art1 = document.getElementsByClassName('art');
-var arts = document.getElementById('arts');
-const officia = document.getElementById('officia')
-//the searchbar
-function searcher() {
-  var a = arts.value.toUpperCase();
-  for (i = 0; i < art1.length; i++) {
-    var h1 = art1[i].getElementsByClassName('ah')[0];
-    var filterer = h1.textContent || h1.innerText;
-    if (filterer.toUpperCase().indexOf(a) > -1) {
-      art1[i].style.display = '';
-    } else {
-      art1[i].style.display = 'none';
-    }
-  }
-}
-const view = document.getElementById('scrol');
-const home = document.getElementById('home');
+about_btn.addEventListener('click',()=>{
+    about_me.scrollIntoView();
+});
 
-const images = document.getElementsByClassName('showcase');
-const dots = document.getElementsByClassName('dot');
-let currentImage = 0;
-function hideImage() {
-  for (i = 0; i < images.length; i++) {
-    images[i].style.display = 'none';
-    dots[i].style.background = 'grey';
-  }
-}
-images[currentImage].style.display = 'inline';
-dots[currentImage].style.background = 'blue';
-const imageSlider = setInterval(() => {
-  hideImage();
-  if (currentImage < images.length - 1) {
-    currentImage++;
-    images[currentImage].style.display = 'inline';
-    dots[currentImage].style.background = 'blue';
-  } else {
-    currentImage = 0;
-    images[currentImage].style.display = 'inline';
-    dots[currentImage].style.background = 'blue';
-  }
-}, 4000);
-//in case I need it as a function
-function slider() {
-  setInterval(() => {
-    hideImage();
-    if (currentImage < images.length - 1) {
-      currentImage++;
-      images[currentImage].style.display = 'inline';
-      dots[currentImage].style.background = 'blue';
-    } else {
-      currentImage = 0;
-      images[currentImage].style.display = 'inline';
-      dots[currentImage].style.background = 'blue';
-    }
-  }, 3000)
-}
-
-
-
-function myEmail() {
-  document.getElementById('social').scrollIntoView();
-}
+projects_btn.addEventListener('click',()=>{
+    projects.scrollIntoView();
+})
